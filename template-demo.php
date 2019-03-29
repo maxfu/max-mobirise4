8c2b5c517dbbf9e07b27df12556071918f47f741
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-29 12:01:38 +0000

    strip_shortcodes

--- template-demo.php
+++ template-demo.php
@@ -12,7 +12,7 @@
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <?php global $post; ?>
                 <?php $output = get_the_content(); ?>
-                <?php // $output = strip_shortcodes($output); ?>
+                <?php $output = strip_shortcodes($output); ?>
                 <?php // $output = apply_filters('wptexturize', $output); ?>
                 <?php // $output = apply_filters('convert_chars', $output); ?>
                 <?php echo $output; ?>
