696ef31002963d7ec1c9048ce0f034c8b380884d
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-29 12:32:41 +0000

    Fix

--- functions.php
+++ functions.php
@@ -673,7 +673,6 @@ function ccca_vcounter_get($condition)
 function ccca_the_excerpt($length_callback = '', $more_callback = '')
 {
     global $post;
-<<<<<<< HEAD
 
     if (function_exists($length_callback)) {
         add_filter('excerpt_length', $length_callback);
@@ -690,13 +689,6 @@ function ccca_the_excerpt($length_callback = '', $more_callback = '')
 //    $output = '<p>' . $output . '</p>';
 //    $output = strip_shortcodes($output); //Strips tags and images
 
-=======
-    $output = get_the_content();
-    $output = preg_replace("~(?:\[/?)[^/\]]+/?\]~s", '', $output);
-    $output = strip_shortcodes($output);
-    $output = apply_filters('wptexturize', $output);
-    $output = apply_filters('convert_chars', $output);
->>>>>>> b2cd3d6fc2c30617d29f5cd6e1626be1ffaf61c1
     echo $output;
 }
 
